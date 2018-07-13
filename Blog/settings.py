afb447ec658c0959014330bba769cd65082a8bc6
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-13 16:32:59 +0300

    better way to solve template path

--- Blog/settings.py
+++ Blog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Blog.urls'
 TEMPLATES = [
 {
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': ['/Users/elijah/desktop/python/Django/Blog/Blog/templates'],
+    'DIRS': [os.path.join(BASE_DIR, 'Blog/templates')],
     'APP_DIRS': True,
     'OPTIONS': {
         'context_processors': [
