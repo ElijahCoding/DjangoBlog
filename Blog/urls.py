d4880fed010cadf12620f0a88a12d88d2de4779a
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-13 17:56:09 +0300

    static file and styles set up

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -8,3 +9,5 @@ urlpatterns = [
     url(r'^$', views.home, name="home"),
     url(r'^about/$', views.about, name="about")
 ]
+
+urlpatterns += staticfiles_urlpatterns()
