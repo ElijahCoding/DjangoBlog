e72c4657bc92d947f590968785401ded55ac57ba
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-17 19:14:12 +0300

    article creation form

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 def article_list(request):
@@ -17,4 +18,5 @@ def article_detail(request, slug):
 
 @login_required(login_url="/accounts/login/")
 def article_create(request):
-    return render(request, 'articles/article_create.html')
+    form = forms.CreateArticle
+    return render(request, 'articles/article_create.html', { 'form': form })
